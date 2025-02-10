0bd2e22b3356889136cc377e7391a0165eb29ad6
v-kuper <vkuper7@gmail.com>
2025-02-10 23:31:39 +0300

    feat: create final app

--- Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/AppDelegate.swift
@@ -2,8 +2,7 @@
 //  AppDelegate.swift
 //  Quizzler-iOS13
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Vitali on 2025.
 //
 
 import UIKit
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,27 +1,56 @@
 //
 //  ViewController.swift
-//  Quizzler-iOS13
+//  Quizzler-iOS
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Vitali on 2025.
 //
 
 import UIKit
+
+struct Question {
+    let question: String
+    let answer: String
+}
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    var questionNumber = 0
+    let quiz: [Question] = [
+        Question(question: "Four + Two is equal to six.", answer: "True"),
+        Question(question: "Five - Three is greater than One", answer: "True"),
+        Question(question: "Three + Eight is less than Ten", answer: "False")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.questionLabel.text = "Four + Two is equal to six?"
+        if !quiz.isEmpty {
+            updateUI()
+        }
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        guard !quiz.isEmpty else { return }
         
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+        
+        print(userAnswer == actualAnswer ? "Correct!" : "Wrong!")
+        
+        nextQuestion()
+    }
+    
+    func nextQuestion() {
+        questionNumber = (questionNumber + 1) % quiz.count
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber].question
+        progressBar.setProgress(Float(questionNumber + 1) / Float(quiz.count), animated: true)
     }
 }
