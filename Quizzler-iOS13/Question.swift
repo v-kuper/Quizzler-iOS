f22a3d418f767e8ea68d66b63f520c78cec0aebc
v-kuper <vkuper7@gmail.com>
2025-02-11 00:45:57 +0300

    feat: add backgroundColor btn

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -11,4 +11,9 @@ import Foundation
 struct Question {
     let question: String
     let answer: String
+    
+    init(q: String, a: String) {
+        self.question = q
+        self.answer = a
+    }
 }
